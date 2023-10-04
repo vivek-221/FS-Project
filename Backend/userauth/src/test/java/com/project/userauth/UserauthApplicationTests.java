7e6d4930803414996d516dae62636ad6d289cc44
“vivek-221 <chagarlamudi.vivek@gmail.com>
2023-10-04 11:45:21 -0400

    Added base code

---
+++ Backend/userauth/src/test/java/com/project/userauth/UserauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.userauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
